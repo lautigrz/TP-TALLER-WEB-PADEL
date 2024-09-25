7c6d34cbf3439c89589e60908e31bd017acb6d45
lautaro <gerez.lautaro12@gmail.com>
2024-09-25 16:32:50 -0300

    test controladorTorneo

--- src/main/java/com/tallerwebi/presentacion/ControladorTorneo.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorTorneo.java
@@ -4,18 +4,23 @@ import com.tallerwebi.dominio.ServicioTorneo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorTorneo {
-    @Autowired
+
     private ServicioTorneo servicioTorneo;
+    @Autowired
+    public ControladorTorneo(ServicioTorneo servicioTorneo) {
+        this.servicioTorneo = servicioTorneo;
+    }
 
     @RequestMapping("/torneo")
-    public String torneos(Model model){
-
-        model.addAttribute("torneos",  this.servicioTorneo.consultasTorneos());
-
-       return "torneo";
+    public ModelAndView torneos(){
+        ModelMap modelo = new ModelMap();
+        modelo.put("torneos", this.servicioTorneo.consultasTorneos());
+       return new ModelAndView("torneo", modelo);
     }
 }
--- src/test/java/com/tallerwebi/presentacion/ControladorTorneoTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorTorneoTest.java
@@ -3,12 +3,22 @@ package com.tallerwebi.presentacion;
 import com.tallerwebi.dominio.ServicioTorneo;
 import com.tallerwebi.dominio.Torneo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ControladorTorneoTest {
 
@@ -18,8 +28,18 @@ public class ControladorTorneoTest {
     @BeforeEach
     public void init(){
        servicioTorneoMock = mock(ServicioTorneo.class);
+       controladorTorneo = new ControladorTorneo(servicioTorneoMock);
     }
 
+    @Test
+    public void debeRetornarLaVistaTorneoConLaListaDeTorneosVacia() {
 
+        when(this.servicioTorneoMock.consultasTorneos()).thenReturn(new ArrayList<>());
+        ModelAndView model = this.controladorTorneo.torneos();
 
+        List<Torneo> torneos = new ArrayList<>();
+
+        assertThat(model.getViewName(), equalToIgnoringCase("torneo"));
+        assertThat(model.getModel().get("torneos"), equalTo(torneos));
+    }
 }
